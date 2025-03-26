16a4bd502fcf482e278f2e430b0108c7d18cd7a6
axi92 <axi92@users.noreply.github.com>
2025-03-26 17:46:45 +0100

    feat!: set default value for metric to 1
    Old value was 0 for all entries

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ ls -lha
 mkdir data
 while true; do
   date
-  hostname=$(hostname) && lsusb -v | jc --lsusb | jq -r '.[] | "lsusb_device{description=\"\(.description)\",id=\"\(.id)\",iProduct=\"\(.device_descriptor.iProduct.description)\",iSerial=\"\(.device_descriptor.iSerial.description)\",_hostname=\"'$hostname'\"} 0 \(now|floor)"' > ./data/usb-prometheus.prom
+  hostname=$(hostname) && lsusb -v | jc --lsusb | jq -r '.[] | "lsusb_device{description=\"\(.description)\",id=\"\(.id)\",iProduct=\"\(.device_descriptor.iProduct.description)\",iSerial=\"\(.device_descriptor.iSerial.description)\",_hostname=\"'$hostname'\"} 1 \(now|floor)"' > ./data/usb-prometheus.prom
   sleep 5
 done
